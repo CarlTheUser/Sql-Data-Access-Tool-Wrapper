487778464aa7291969fb5be28900ec67b1896520
Carl Andres <carlandres18@gmail.com>
2019-01-13 14:51:41 +0800

    Added class containing factory methods for sql providers

---
+++ DataAccess.Sql/SqlProviderFactory.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Sql.Provider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Sql
+{
+    public class SqlProviderFactory
+    {
+        public static ISqlProvider CreateMsSqlProvider(string connectionString)
+        {
+            return new SqlServerProvider(connectionString);
+        }
+
+        public static ISqlProvider CreateMySqlProvider(string connectionString)
+        {
+            return new MySqlProvider(connectionString);
+        }
+
+        public static ISqlProvider CreateOleDbSqlProvider(string connectionString)
+        {
+            return new OleDbProvider(connectionString);
+        }
+    }
+}
